ecf330010ae225e2c59c5c5b7b17651f10b4bebc
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-04 16:37:57 +0700

    use viewmodel

--- app/src/main/java/dev/chrsep/caniuse/components/CaniuseRoot.kt
+++ app/src/main/java/dev/chrsep/caniuse/components/CaniuseRoot.kt
@@ -6,14 +6,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.viewinterop.viewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import androidx.ui.tooling.preview.Preview
+import dev.chrsep.caniuse.db.EraDao
+
+
+class CaniuseRootViewModel @ViewModelInject constructor(eraDao: EraDao) : ViewModel() {
+    val text = "asdf"
+}
 
 @Composable
 fun CaniuseRoot() {
+    val viewModel: CaniuseRootViewModel = viewModel()
+
     MaterialTheme {
         Scaffold {
-            Greeting("Android")
+            Greeting(viewModel.text)
         }
     }
+}
+
+@Preview
+@Composable
+fun CaniusePreview() {
+    CaniuseRoot()
 }
 
 @Composable
